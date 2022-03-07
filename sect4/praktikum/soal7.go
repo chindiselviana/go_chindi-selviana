3d818027d32161d08c41f2a279b371cddf8fc1ff
chindi selviana <selvianachiindi@gmail.com>
2022-03-07 18:32:02 +0700

    upload tugas

---
+++ sect4/praktikum/soal7.go
@@ -0,0 +1,31 @@
+[17.33, 7/3/2022] Indah Lestari: package main
+
+import "fmt"
+
+func playWithAsterik(n int) {
+	//variabel
+	var asterik string
+	var star string = " * "
+
+	//code
+	for i := 1; 1 <= n; i++ {
+
+		for j := n - 1; j >= 1; j-- {
+		}
+		asterik += star
+		fmt.Println(asterik)
+	}
+}
+
+func main() {
+	playWithAsterik(5)
+	/*
+
+			   *
+			 *  *
+		    *  *  *
+		   *  *  * *
+		  * *  * *  *
+
+	*/
+}
